1c890b6972ee0dd167808e245c4d61e6d6f498ad
Boldhonor <ssunil@ch.iitr.ac.in>
2021-02-06 20:58:48 +0530

    Final Settings

---
+++ HomingosAssignment1/Assets/Files/Gallery buttons.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gallerybuttons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void next()
+    {
+        this.transform.Translate(-16, 0, 0);
+    }
+}
---
+++ HomingosAssignment1/Assets/gallerychange.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class gallerychange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    static int totalMemories = 5;
+    [SerializeField] GameObject[] memories = new GameObject[totalMemories];
+    int currentMemory =0;
+    
+    void Start()
+    {
+        Debug.Log("safd");
+        Debug.Log(memories.Length);
+        foreach(GameObject memo in memories)
+        {
+            memo.GetComponent<MeshRenderer>().enabled = false;
+            
+        }
+        memories[currentMemory].GetComponent<MeshRenderer>().enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void next()
+    {
+        if (currentMemory == totalMemories - 1) return;
+        memories[currentMemory].GetComponent<MeshRenderer>().enabled = false;
+        currentMemory++;
+        memories[currentMemory].transform.position = new Vector3(0,0.4f,0);
+        Debug.Log("safd");
+        Debug.Log(memories.Length);
+        return;
+        
+
+
+    }
+}
