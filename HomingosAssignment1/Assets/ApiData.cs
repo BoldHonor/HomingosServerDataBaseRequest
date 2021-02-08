6a639c1b96b2a92ae54d8dfd64404062be7a17e7
Boldhonor <ssunil@ch.iitr.ac.in>
2021-02-09 03:20:38 +0530

    Data retrive

---
+++ HomingosAssignment1/Assets/ApiData.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApiData : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [System.Serializable]
+    public class MemoData
+    {
+        public string type;
+        public string url;
+        public string caption;
+    }
+    [System.Serializable]
+    public class Resources
+    {
+        public MemoData[] resources;
+    }
+
+    public TextAsset jsonFile;
+
+    void Start()
+    {
+
+        string str = File.ReadAllText(Application.dataPath + "/data.txt");
+        Debug.Log(str);
+        Resources res = JsonUtility.FromJson<Resources>(jsonFile.text);
+        Debug.Log(res.resources[1].type);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   
+}
---
+++ HomingosAssignment1/Assets/ApiDataReciver.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AoiDataReciver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        memoryData mem = new memoryData();
+        mem.type = "video";
+        mem.url = "http//htrp";
+        mem.caption = "let it be ";
+        string data = JsonUtility.ToJson(mem);
+        Debug.Log(data);
+        File.WriteAllText(Application.dataPath + "/memores.json", data);
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    public class memoryData
+    {
+        public string type;
+        public string url;
+        public string caption;
+    }
+}
