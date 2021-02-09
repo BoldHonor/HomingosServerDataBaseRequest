31360a72305e0f7b9b222641f91e3cf9ab9e8595
Boldhonor <ssunil@ch.iitr.ac.in>
2021-02-09 08:55:03 +0530

    Final real time api retrival

--- HomingosAssignment1/Assets/ApiData.cs
+++ HomingosAssignment1/Assets/ApiData.cs
@@ -3,9 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.Video;
 public class ApiData : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+
+    //classes-----------------------------------------------------------------------------
     [System.Serializable]
     public class MemoData
     {
@@ -14,35 +17,143 @@ public class ApiData : MonoBehaviour
         public string caption;
     }
     [System.Serializable]
-    public class Resources
+    public class Res
     {
         public MemoData[] resources;
     }
 
+    //variables-----------------------------------------------------------
     public TextAsset jsonFile;
-    string retiriveData = "https://run.mocky.io/v3/6027ded7-2411-4fb0-8433-efc50814abd7" ;
-
-
+    
+    static public Res res;
+    public GameObject GalleryCanvas;
+    [SerializeField] GameObject caption;
+    MeshRenderer GalleryRender;
+    VideoPlayer GalleryPlayer;
+    int currentMedia = 0;
+    int previousMedia = 2;
+    bool stateChange = false;
+    bool previousState =false;
+    public Texture2D loading;
+    
+    //methods--------------------------------------------------------------
     void Start()
     {
-        UnityWebRequest retrivedRequestData = UnityWebRequest.Get(retiriveData);
-        if(retrivedRequestData.isNetworkError)
-        {
-            Debug.Log("cant retrive Data"); 
-        }
-
-        string str = File.ReadAllText(Application.dataPath + "/data.txt");
-        Debug.Log(retrivedRequestData.downloadHandler.text);
-        retrivedRequestData.Send();
-        Resources res = JsonUtility.FromJson<Resources>(retrivedRequestData.downloadHandler.text);
-        Debug.Log(res.resources[1].type);
+        GalleryRender = GalleryCanvas.GetComponent<MeshRenderer>();
+        GalleryPlayer = GalleryCanvas.GetComponent<VideoPlayer>();
+        GalleryPlayer.enabled = false;
+        StartCoroutine(RetriveApiData());
+        Debug.Log(res.resources.Length);
         
+        Display();
     }
-
-    // Update is called once per frame
     void Update()
     {
+        if(GalleryRender.enabled !=previousState)               // minimize load by checking change in state
+        {
+            previousState = GalleryRender.enabled;
+            if(GalleryRender.enabled && previousMedia != currentMedia)                           // proceed if visible
+            {
+                previousMedia = currentMedia;
+                
+                Display();
+
+               
+            }
+
+            if (!GalleryRender.enabled)                                                                //stops the video When not displayed 
+            {
+                clear();
+            }
+
+
+        }
+
+    }
+
+    void Display()                                                                      //Displays Current Gallery Element 
+    {
+        caption.GetComponent<UnityEngine.UI.Text>().text = res.resources[currentMedia].caption;
+
+        if (res.resources[currentMedia].type == "image")
+        { StartCoroutine(MediaRetrive()); }
+
+        if (res.resources[currentMedia].type == "video")
+        {
+            GalleryPlayer.enabled = true;
+            videoMedia();
+            GalleryPlayer.Play();
+        }
+    }
+
+    public void next()                                                          //next button
+    {
+        if (currentMedia == res.resources.Length - 1) return;
+        clear();
+        currentMedia++;
+        Debug.Log(res.resources.Length);
+        StopCoroutine(MediaRetrive());
+        Display();
+
+    }
+
+    public void back()                                                          //prevous button
+    {
+        if (currentMedia == 0) return;
+        clear();
+        currentMedia--;
+        StopCoroutine(MediaRetrive());
+        Display();
+
+    }
+
+    void videoMedia()                                                       //loads video in player
+    {
+        GalleryPlayer.url = res.resources[currentMedia].url;
         
     }
-   
+
+    void clear()                                                                //clears screen 
+    {
+        GalleryPlayer.Stop();
+        GalleryPlayer.enabled = false;
+        GalleryRender.material.mainTexture = loading;
+    }
+
+
+    //corutines------------------------------------------------------------
+    IEnumerator RetriveApiData()
+    {
+        UnityWebRequest retrivedRequestData = UnityWebRequest.Get(MainMenu.retiriveData);
+         yield return retrivedRequestData.Send();
+
+        if (retrivedRequestData.isNetworkError) { caption.GetComponent<UnityEngine.UI.Text>().text = "Loading..."; }
+        else
+        {
+            if (retrivedRequestData.isDone)
+            {
+
+
+                Debug.Log("retrived ");
+                Debug.Log(retrivedRequestData.downloadHandler.text);
+                res = JsonUtility.FromJson<Res>(retrivedRequestData.downloadHandler.text);
+                Debug.Log(res.resources[1].type);
+                Debug.Log(res.resources.Length);
+                Display();
+
+            }
+        }
+    }
+
+
+    IEnumerator MediaRetrive()
+    {
+        WWW media = new WWW(res.resources[currentMedia].url);
+        yield return media;
+        GalleryRender.material.mainTexture = media.texture;
+
+
+    }
+
+
 }
---
+++ HomingosAssignment1/Assets/MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Variables -----------------------------------------------------------------------
+    static public string retiriveData;
+    [SerializeField]public GameObject UrlText;
+
+    //methods -------------------------------------------------------------------------
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onAppStart()
+    {
+        if(UrlText.GetComponent <UnityEngine.UI.Text>().text == "")
+        {
+            retiriveData = "https://run.mocky.io/v3/6027ded7-2411-4fb0-8433-efc50814abd7";
+
+        }
+        else
+        {
+            retiriveData = UrlText.GetComponent<UnityEngine.UI.Text>().text;
+        }
+
+        SceneManager.LoadScene("PrakharHomingos Assignment");
+    }
+}
